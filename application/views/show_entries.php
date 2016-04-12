68682bad7a36cc074bae0e010d118829d85a2823
Miquel Angel Caldentey <mcaldentey@kitmaker.com>
2016-04-12 14:26:22 +0200

    Separadas las Entrys en la pantalla principal

--- application/views/show_entries.php
+++ application/views/show_entries.php
@@ -30,25 +30,25 @@
 <body>
     <?php include('menu.php');?>    
 
-    <div class="entries">  
-        <?php if (!empty($entries)) : ?> <!-- Shows all the entries -->
-            <?php foreach($entries as $entry) : ?>
-                <?php                            
-                $edit   = ' '; 
-                $delete = ' ';   
-                
-                if ($this -> session -> userdata('is_logged_in')){
+    <?php if (!empty($entries)) : ?> <!-- Shows all the entries -->
+        <?php foreach($entries as $entry) : ?>
+           <div class="entries">  
+            <?php                            
+            $edit   = ' '; 
+            $delete = ' ';   
 
-                    $edit   = 'Edit'; 
-                    $delete = 'Delete';                            
-                }
-                ?>
-                
+            if ($this -> session -> userdata('is_logged_in')){
+
+                $edit   = 'Edit'; 
+                $delete = 'Delete';                            
+            }
+            ?>
+            <div class="head_entry">
                 <?php echo'<a href="'.'index.php/blog/view/'.$entry -> id.'"><img src="'.$entry -> image.'" class="resizeSmall" /></a>' ?>   
                 <h2><a><?=anchor(base_url().'index.php/blog/view/'.$entry -> id, $entry -> title)?></a></h2> <!-- Load the entry image and the title --> 
-
-                <div class="h3">
-                    <?php  
+            </div>
+            <div class="h3">
+                <?php  
                     if ($this -> session -> userdata('username') == 'admin') { //If the admin is conected, displays the options
                         echo '<div id="menu" class="menu">';
                         echo '<h5>Options</h5>';
@@ -73,27 +73,27 @@
                 </h3>
                 <hr/>
 
+            </div>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <h1>No entries</h1>
+    <?php endif; ?>
 
-            <?php endforeach; ?>
-        <?php else : ?>
-            <h1>No entries</h1>
-        <?php endif; ?>
-
-        <?php
+    <?php
                 // Include the autoloader
-        require_once 'wurfl/src/autoload.php'; 
+    require_once 'wurfl/src/autoload.php'; 
                 // Create a configuration object  
-        $config = new ScientiaMobile\WurflCloud\Config();  
+    $config = new ScientiaMobile\WurflCloud\Config();  
                 // Set your WURFL Cloud API Key  
-        $config->api_key = '506180:tOpobuIFCjlmTaRa2WvbxZx4fe7QpTb6';   
+    $config->api_key = '506180:tOpobuIFCjlmTaRa2WvbxZx4fe7QpTb6';   
                 // Create the WURFL Cloud Client  
-        $client = new ScientiaMobile\WurflCloud\Client($config);  
+    $client = new ScientiaMobile\WurflCloud\Client($config);  
                 // Detect your device  
-        $client->detectDevice();  
+    $client->detectDevice();  
                 // Use the capabilities  
-        echo '<div class="deviceMobile">Conected from: '.$client->getDeviceCapability('complete_device_name').'</div>';                  
-        ?>       
-    </div>
-    <?php include('footer.php');?>    
+    echo '<div class="deviceMobile">Conected from: '.$client->getDeviceCapability('complete_device_name').'</div>';                  
+    ?>       
+</div>
+<?php include('footer.php');?>    
 </body>
 </html>
